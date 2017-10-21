3b9723ebdc1200c9335513e68d8e3196f6d04a88
inomyabcs <32985654+inomyabcs@users.noreply.github.com>
2017-10-21 17:35:20 -0400

    Update compress.js
    Added missing mime type

--- compress.js
+++ compress.js
@@ -10,7 +10,7 @@ function compress(source_img_obj) {
     cvs.width = source_img_obj.naturalWidth;
     cvs.height = source_img_obj.naturalHeight;
     var ctx = cvs.getContext("2d").drawImage(source_img_obj, 0, 0);
-    var newImageData = cvs.toDataURL(mime_type, 0.5);
+    var newImageData = cvs.toDataURL("image/jpeg", 0.5);
     var result_image_obj = new Image();
     result_image_obj.src = newImageData;
 	input.files[0] = result_image_obj;
