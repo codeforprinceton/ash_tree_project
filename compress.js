ce18b0873a3e7ea5ee5d9c9d03467b6ac63a5de6
inomyabcs <32985654+inomyabcs@users.noreply.github.com>
2017-10-21 17:27:18 -0400

    Compression of images
    Needs to be called in index.html form and tested

---
+++ compress.js
@@ -0,0 +1,17 @@
+var input = document.querySelector("input[type=file]");
+
+input.addEventListener("change", function () {
+  var file = input.files[0];
+  drawOnCanvas(file);
+};
+
+function compress(source_img_obj) {	
+	var cvs = document.createElement("canvas");
+    cvs.width = source_img_obj.naturalWidth;
+    cvs.height = source_img_obj.naturalHeight;
+    var ctx = cvs.getContext("2d").drawImage(source_img_obj, 0, 0);
+    var newImageData = cvs.toDataURL(mime_type, 0.5);
+    var result_image_obj = new Image();
+    result_image_obj.src = newImageData;
+	input.files[0] = result_image_obj;
+}
