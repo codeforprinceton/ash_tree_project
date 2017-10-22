bf913e9f6aeea7e457045cc82e1282c9cd2ca359
Hanson Yu <hansoncyu@gmail.com>
2017-10-22 12:10:42 -0400

    added alert for input files

--- contentful_script.js
+++ contentful_script.js
@@ -84,6 +84,7 @@ var img_name;
 input.addEventListener("change", function (e) {
   img_name = input.files[0].name;
   console.log(input.files[0]);
+  alert(input.files[0].size);
   var label = $(e.target).prev();
   label.css("border", "green solid 2px");
   createImageBitmap(input.files[0])
